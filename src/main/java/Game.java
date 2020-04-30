5762bd596fb45f1d88cc05e832ac8034f708db74
flaviacarvalhido <flaviacarvalhido@gmail.com>
2020-04-30 11:17:26 +0100

    added maps and map reader

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 
 public class Game {
     public static void main(String[] args) throws IOException {
-        ArenaModel arena = new ArenaModel(60, 30);
+        //ArenaModel arena = new ArenaModel(60, 30);
+        //ArenaModel arena = new ArenaModel(60, 30,"C:\\Users\\shit\\OneDrive\\Documentos\\LPOO\\proj\\src\\main\\java\\files\\mapMedium.txt");
+        ArenaModel arena = new ArenaModel(60, 30,"C:\\Users\\shit\\OneDrive\\Documentos\\LPOO\\proj\\src\\main\\java\\files\\mapHard.txt");
         ArenaView gui = new ArenaView(60, 35);
 
         ArenaController controller = new ArenaController(gui, arena);

80084d7b8341d3d614014517081a4cdc25dab028
flaviacarvalhido <flaviacarvalhido@gmail.com>
2020-05-31 01:38:46 +0100

    finished command tests

--- src/main/java/commands/NullCommand.java
+++ src/main/java/commands/NullCommand.java
@@ -6,13 +6,8 @@ import model.MenuModel;
 public class NullCommand extends Command implements CommandMenu, CommandArena{
 
     @Override
-    public void executeMenu(MenuModel menuModel) {
-
-
-    }
+    public void executeMenu(MenuModel menuModel) {}
 
     @Override
-    public void executeArena(ArenaModel arenaModel) {
-
-    }
+    public void executeArena(ArenaModel arenaModel) {}
 }
